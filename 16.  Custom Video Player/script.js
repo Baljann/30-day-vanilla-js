8d68252aa6792e68f95f30b5c4b493be9917bb0f
Baljan Kanatbek <baljann19@gmail.com>
2025-03-27 22:54:56 +0300

    feat: add viewport meta tag and link stylesheets in index.html; create script.js for video player functionality

---
+++ 16.  Custom Video Player/script.js
@@ -0,0 +1,13 @@
+/*1. Get Elenments*/
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelector("[data-skip]");
+const ranges = player.querySelector(".player__slider");
+
+/*2. Build Functions*/
+
+/*3. Hook up the event listeners*/
